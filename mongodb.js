c0e952e9637792a05deb481b2fa0f1041ecaa1f5
Chima <onyiaguchima@gmail.com>
2022-09-12 15:00:12 +0100

    m

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,10 @@
 const mongodb = require("mongodb");
-const {MongoClient} = mongodb;
+const {MongoClient, ObjectId} = mongodb;
 const connectionurl = "mongodb://127.0.0.1:27017";
 const databasename = "taskmanager2";
+// const id = new ObjectId()
+// console.log(id);
+// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionurl, {useNewUrlParser: true}, (error, client)=>{
     if(error){
@@ -9,8 +12,11 @@ MongoClient.connect(connectionurl, {useNewUrlParser: true}, (error, client)=>{
     }
 
     const db = client.db(databasename);
+
+        
     // db.collection("users").insertOne({
-    //     name: "",
+    //     _id: id,
+    //     name: "Wizzy",
     //     age: 39,
     //     nationality: "Nigerian"
     // }, (error, result)=>{
@@ -21,11 +27,11 @@ MongoClient.connect(connectionurl, {useNewUrlParser: true}, (error, client)=>{
     //     console.log(result);
     // });
 
-    db.collection("players").insertMany([{club: "wolve", role: "striker", number: 9, name: "raul", goals: 30}, {club: "chelsea", role: "striker", number: 9, name: "sterling", goals: 11}], (error, response)=>{
-        if(error){
-            return console.log("an error was encountered")
-        }
-        console.log(result)
-    });
+    // db.collection("players").insertMany([{club: "wolve", role: "striker", number: 9, name: "raul", goals: 30}, {club: "chelsea", role: "striker", number: 9, name: "sterling", goals: 11}], (error, response)=>{
+    //     if(error){
+    //         return console.log("an error was encountered")
+    //     }
+    //     console.log(result)
+    // });
 }); 
 
